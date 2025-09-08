2c3120f189e466971828d8dca725435678bf2214
asrithreddy755 <sattiasrithreddy@gmail.com>
2025-09-08 22:07:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -274,5 +274,6 @@ def home():
     return "Hello from Flask on Render!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+     create_tables()
+    app.run(host="0.0.0.0", port=5000)
+
